5c87f97f6e320c2ee73dc9ffc00747a9c6e59e30
felipendc <felipendc10@gmail.com>
2022-02-22 06:25:50 -0300

    Vicyos Rolling Update: (Tue Feb 22 06:25:46 -03 2022)

--- wsl-android-build-environment-setup/android_build_env.sh
+++ wsl-android-build-environment-setup/android_build_env.sh
@@ -7,11 +7,6 @@
 # Script to setup an AOSP Build environment on Ubuntu and Linux Mint
 
 LATEST_MAKE_VERSION="4.3"
-UBUNTU_16_PACKAGES="libesd0-dev"
-UBUNTU_20_PACKAGES="libncurses5 curl python-is-python3"
-DEBIAN_10_PACKAGES="libncurses5"
-DEBIAN_11_PACKAGES="libncurses5"
-PACKAGES=""
 
 echo "Adding GitHub apt key and repository!"
 sudo apt install software-properties-common -y
@@ -19,35 +14,106 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 
 sudo apt update
-
 # Install lsb-core packages
 sudo apt install lsb-core -y
 
-LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
-
-if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
-    PACKAGES="${UBUNTU_16_PACKAGES}"
-elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" || ${LSB_RELEASE} =~ "Ubuntu 21" ]]; then
-    PACKAGES="${UBUNTU_20_PACKAGES}"
-elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
-    PACKAGES="${DEBIAN_10_PACKAGES}"
-elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 11" ]]; then
-    PACKAGES="${DEBIAN_11_PACKAGES}"
-fi
+# Install packages
+sudo apt install git -y
+sudo apt install libncurses5
+sudo apt install libesd0-dev
+sudo apt install gnupg -y
+sudo apt install flex -y
+sudo apt install bison-y
+sudo apt install gperf -y
+sudo apt install build-essential -y
+sudo apt install zip -y
+sudo apt install curl -y
+sudo apt install zlib1g-dev -y
+sudo apt install gcc-multilib -y
+sudo apt install g++-multilib -y
+sudo apt install libc6-dev-i386 -y
+sudo apt install lib32ncurses5-dev -y
+sudo apt install x11proto-core-dev -y
+sudo apt install libx11-dev -y
+sudo apt install lib32z-dev -y
+sudo apt install lib32z1-dev -y
+sudo apt install ccache -y
+sudo apt install libgl1-mesa-dev -y
+sudo apt install libxml2 -y
+sudo apt install libxml2-utils -y
+sudo apt install xsltproc -y
+sudo apt install unzip -y
+sudo apt install squashfs-tools -y
+sudo apt install python -y
+sudo apt install python-mako -y
+sudo apt install libssl-dev -y
+sudo apt install ninja-build -y
+sudo apt install lunzip -y
+sudo apt install syslinux -y
+sudo apt install syslinux-utils -y
+sudo apt install gettext -y
+sudo apt install genisoimage -y
+sudo apt install bc -y
+sudo apt install xorriso -y
+sudo apt install liblz4-tool -y
+sudo apt install libncurses5-dev -y
+sudo apt install libsdl1.2-dev -y
+sudo apt install libwxgtk3.0-dev -y
+sudo apt install lzop -y
+sudo apt install maven -y
+sudo apt install pngcrush -y
+sudo apt install schedtool -y
+sudo apt install lib32readline-dev -y
+sudo apt install imagemagick -y
+sudo apt install rsync -y
+sudo apt install adb -y
+sudo apt install autoconf -y
+sudo apt install automake -y
+sudo apt install axel -y
+sudo apt install bc -y
+sudo apt install clang -y
+sudo apt install cmake -y
+sudo apt install expat -y
+sudo apt install fastboot -y
+sudo apt install g++ -y
+sudo apt install gawk -y
+sudo apt install gcc -y
+sudo apt install htop -y
+sudo apt install libtinfo5 -y
+sudo apt install libc6-dev -y
+sudo apt install libcap-dev -y
+sudo apt install libexpat1-dev -y
+sudo apt install libgmp-dev -y
+sudo apt install '^liblz4-.*' -y
+sudo apt install '^liblzma.*' -y
+sudo apt install libmpc-dev -y
+sudo apt install libmpfr-dev -y
+sudo apt install libncurses5-dev -y
+sudo apt install libtool -y
+sudo apt install '^lzma.*' -y
+sudo apt install ncftp -y
+sudo apt install ncurses-dev -y
+sudo apt install patch -y
+sudo apt install patchelf -y
+sudo apt install pkg-config -y
+sudo apt install pngquant -y
+sudo apt install python2.7 -y
+sudo apt install python-all-dev -y
+sudo apt install re2c -y
+sudo apt install schedtool -y
+sudo apt install subversion -y
+sudo apt install texinfo -y
+sudo apt install w3m -y
+sudo apt install xsltproc -y
+sudo apt install lzip -y
+sudo apt install libxml-simple-perl -y
+sudo apt install libswitch-perl -y
+sudo apt install apt-utils -y
+sudo apt install gh -y
+sudo apt install -y
 
 sudo DEBIAN_FRONTEND=noninteractive \
-    apt install \
-    git gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib \
-    g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev \
-    lib32z-dev lib32z1-dev ccache libgl1-mesa-dev libxml2 libxml2-utils xsltproc unzip \
-    squashfs-tools python python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils \
-    gettext genisoimage bc xorriso liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev \
-    lzop maven pngcrush schedtool lib32readline-dev imagemagick rsync adb autoconf automake \
-    axel bc clang cmake expat fastboot g++ gawk gcc htop libtinfo5 libc6-dev libcap-dev \
-    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
-    libtool '^lzma.*' ncftp ncurses-dev patch patchelf pkg-config pngquant python2.7 python-all-dev \
-    re2c schedtool subversion texinfo w3m xsltproc lzip libxml-simple-perl libswitch-perl apt-utils gh \
-    ${PACKAGES} -y
+    apt install ${PACKAGES} -y
 
 echo -e "Setting up udev rules for adb!"
 sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
